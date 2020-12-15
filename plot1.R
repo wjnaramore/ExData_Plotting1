34875820e8a91ee614259dcb548d7b5827c5267d
wjnaramore <wjnaramore@gmail.com>
2020-12-14 23:11:43 -0500

    adding data import to plot1,2,3,4 R code
    adding data import to plot1,2,3,4 R code, adding zip file

--- plot1.R
+++ plot1.R
@@ -1,3 +1,11 @@
+library(dplyr)
+
+unzip("exdata_data_household_power_consumption.zip")
+data <- read.table("household_power_consumption.txt", sep = ";", na.strings = "?", header = TRUE)
+date_range <- as.Date(c("01/02/2007","02/02/2007"),format = "%d/%m/%Y")
+data2day <- subset(data,as.Date(data$Date, format = "%d/%m/%Y") >= date_range[1] & as.Date(data$Date, format = "%d/%m/%Y") <= date_range[2])
+data2day<-mutate(data2day,DateTime = strptime(paste(data2day$Date,data2day$Time),format = "%d/%m/%Y %H:%M:%S"))
+
 png(filename = "plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
 hist(data2day$Global_active_power,xlab = "Global Active Power (kilowatts)",main = "Global Active Power",col = "red")
 dev.off()
